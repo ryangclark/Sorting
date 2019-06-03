d34cec458244d6a64b70c0732dc61aa9d07cc562
Clark <rclark010@gmail.com>
2019-06-03 15:17:45 -0600

    Completes challenges

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,53 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+   for index, item in enumerate(arr):
+      if item == target:
+         return index
 
    return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
-  if len(arr) == 0:
-    return -1 # array empty
+   if len(arr) == 0:
+      return -1 # array empty
     
-  low = 0
-  high = len(arr)-1
+   low = 0
+   high = len(arr)-1
+   middle = (low + high) // 2
+   middle_value = arr[middle]
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+   while True:
+      if middle_value == target:
+         return middle
+      elif high - low == 1:
+         return -1 # not found
+      elif middle_value < target:
+         low = middle
+         middle = (low + high) // 2
+         middle_value = arr[middle]
+      elif middle_value > target:
+         high = middle
+         middle = (low + high) // 2
+         middle_value = arr[middle]
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+   if len(arr) == 0:
+      return -1 # array empty
+
+   middle = (low+high)//2
+   middle_value = arr[middle]
+
+   if middle_value == target:
+      return middle
+   elif high - low == 1:
+      return -1 # not found
+   elif middle_value < target:
+      return binary_search_recursive(arr, target, middle, high)
+   elif middle_value > target:
+      return binary_search_recursive(arr, target, low, middle)
+      
